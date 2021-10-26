b313e442385c64285a52ca62048be1557036395a
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-10-26 22:25:35 +0300

    added home page and view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
